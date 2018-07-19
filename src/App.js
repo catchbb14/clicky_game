6e7770f4507d9c85d82b8ed1c480781d575f3b77
Eric Morrow <ericmorrow14@gmail.com>
2018-07-18 21:47:13 -0400

    Added click handling and score incrementation to app.js. Added components to be rendered

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PlayerCard from "./components/PlayerCard";
+import NavBar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
 import './App.css';
 import friends from "./friends.json";
 
@@ -12,27 +14,72 @@ class App extends Component {
     currScore: 0,
     topScore: 0,
     chosen: [],
-    message: ""
+    message: "Click a player to begin the game!"
   };
 
   shuffleList = () => {
     this.setState({ friends: shuffle(friends) })
   };
 
-  resetGame = () => {
+  resetGame = (gameMessage) => {
     this.setState({
       currScore: 0,
-      topScore = this.state.currScore,
-      chosen = [],
-      message = "You have already chosen that one, try again!"
+      chosen: [],
+      message: gameMessage
     });
     this.shuffleList();
+  }
+
+  handleClick = id => {
+    if(this.state.chosen.includes(id)) {
+      this.resetGame("Try again! You have already chosen him.");
+    } else {
+      this.handleCorrect();
+      this.setState({ chosen: [...this.state.chosen, id] })
+    }
+  }
+
+  handleCorrect = () => {
+    const newScore = this.state.currScore + 1;
+    this.shuffleList();
+
+    this.setState({
+      currScore: newScore,
+      message: ""
+    })
+
+    if(newScore > this.state.topScore) {
+      this.setState({ topScore: newScore })
+    }
+    if(newScore === 12) {
+      this.resetGame("You won!");
+    }
   }
 
   render() {
     return (
       <Wrapper>
-        
+        <NavBar
+          message = {this.state.message}
+          currScore = {this.state.currScore}
+          topScore = {this.state.topScore}
+        />
+        <div className = "container">
+          {this.state.friends.map(friend => (
+            <PlayerCard
+              shuffleList = {this.shuffleList}
+              resetGame = {this.resetGame}
+              handleClick = {this.handleClick}
+              handleCorrect = {this.handleCorrect}
+              id = {friend.id}
+              key = {friend.id}
+              name = {friend.name}
+              number = {friend.number}
+              position = {friend.position}
+              image = {friend.image}
+            />
+          ))}
+        </div> 
       </Wrapper>
     );
   }
