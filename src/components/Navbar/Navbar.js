78dc3d23a1ee54443da8d57e9da633d9cb45e3f3
Eric Morrow <ericmorrow14@gmail.com>
2018-07-18 21:45:58 -0400

    Completed navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "./Navbar.css"
 
 const Navbar = props => (
-    <nav className="navbar">
-        <ul>
-            <li class="brand"><a href="/">The Red Sox</a></li>
-            <li>{props.message}</li>
-            <li>Score: {props.currScore} | Top Score: {props.topScore}</li>
-        </ul>
-    </nav>
+    
+    <div className="navbar row">
+        <div className = "col-3 brand text-center"><img alt="Red Sox Logo" src="/images/logo.png" /></div>
+            <div className = "col-6 text-center message"><h3>{props.message}</h3></div>
+            <div className = "col-3 text-center score"><h4>Score: {props.currScore} | Top Score: {props.topScore}</h4></div>
+
+    </div>
 );
 
 export default Navbar;
